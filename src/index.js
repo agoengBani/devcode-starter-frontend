b65a4b791be03810b60d1c89c8dd4f7a310a24fe
agoengBani <agungsyabani99@gmail.com>
2024-07-07 09:36:32 +0700

    challange 1

--- src/index.js
+++ src/index.js
@@ -5,27 +5,27 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 // TODO: Uncomment baris kode dibawah ini untuk meng-import Halaman Hello ke dalam router
-// import Hello from "./routes/hello";
+import Hello from "./routes/hello";
 import Home from "./routes/home";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
+   {
+      path: "/",
+      element: <Home />,
+   },
 
-  // TODO: Uncomment baris kode dibawah ini untuk menambahkan routing baru ke Halaman Hello
-  // {
-  //   path: "/hello",
-  //   element: <Hello />,
-  // },
+   // TODO: Uncomment baris kode dibawah ini untuk menambahkan routing baru ke Halaman Hello
+   {
+      path: "/hello",
+      element: <Hello />,
+   },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} className="app" />
-  </React.StrictMode>
+   <React.StrictMode>
+      <RouterProvider router={router} className="app" />
+   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
