6601a0b973cb3c1c56e16b7e7cebbc5204897612
agoengBani <agungsyabani99@gmail.com>
2024-07-07 09:31:28 +0700

    first commit

---
+++ src/components/HelloWorld/index.js
@@ -0,0 +1,15 @@
+// TODO: Uncomment baris kode di bawah ini untuk meng-import gambar logo devcode yang berada di dalam folder assets
+// import devcodeLogo from "../../assets/devcode-logo.png";
+import "./style.css";
+
+export const HelloWorld = () => {
+  return (
+    <div className="hello">
+      {/* TODO: Tambahkan gambar logo devcode yang sudah diimport sebelumnya ke dalam properti src pada elemen img di bawah ini */}
+      {/* <img data-cy="devcode-logo" src={} alt="devcode logo" /> */}
+      <div data-cy="devcode-title">
+        {/* TODO: Tambahkan elemen heading 1 di bawah ini dengan berisi kalimat I'm ready for the next challenge! */}
+      </div>
+    </div>
+  );
+};
